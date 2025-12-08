d0049ca67b3229fc8d65c9ea4d8137c723f0ca7e
Devron Tombacco <tombacco.devron@gmail.com>
2025-12-08 19:01:28 +0000

    Add all 3 VPCs in same region

--- main.tf
+++ main.tf
@@ -3,3 +3,30 @@ provider "aws" {
   region = "eu-west-1"
 }
 
+
+resource "aws_vpc" "vpc-a" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "vpc-a"
+  }
+
+}
+
+resource "aws_vpc" "vpc-b" {
+  cidr_block = "10.1.0.0/16"
+
+  tags = {
+    Name = "vpc-b"
+  }
+
+}
+
+resource "aws_vpc" "vpc-c" {
+  cidr_block = "10.2.0.0/16"
+
+  tags = {
+    Name = "vpc-c"
+  }
+
+}
