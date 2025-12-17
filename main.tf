28a4a5d30f7246df56a040c399c2d7f0dccb8ea3
Devron Tombacco <tombacco.devron@gmail.com>
2025-12-17 11:59:05 +0000

    refactor: move VPC resources from main.tf to vpcs.tf

--- main.tf
+++ main.tf
@@ -4,15 +4,6 @@ provider "aws" {
 }
 
 
-resource "aws_vpc" "vpc-a" {
-  cidr_block = "10.0.0.0/16"
-
-  tags = {
-    Name = "vpc-a"
-  }
-
-}
-
 resource "aws_subnet" "subnet-1a" {
   vpc_id                  = aws_vpc.vpc-a.id
   cidr_block              = "10.0.1.0/24"
@@ -23,14 +14,6 @@ resource "aws_subnet" "subnet-1a" {
   }
 }
 
-resource "aws_vpc" "vpc-b" {
-  cidr_block = "10.1.0.0/16"
-
-  tags = {
-    Name = "vpc-b"
-  }
-
-}
 
 resource "aws_subnet" "subnet-1b" {
   vpc_id                  = aws_vpc.vpc-b.id
@@ -42,14 +25,6 @@ resource "aws_subnet" "subnet-1b" {
   }
 }
 
-resource "aws_vpc" "vpc-c" {
-  cidr_block = "10.2.0.0/16"
-
-  tags = {
-    Name = "vpc-c"
-  }
-
-}
 
 resource "aws_subnet" "subnet-1c" {
   vpc_id                  = aws_vpc.vpc-c.id
