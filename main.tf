9e06299a4009d977789e621479f696926bff598e
Devron Tombacco <tombacco.devron@gmail.com>
2025-12-17 10:44:50 +0000

    correct error for subnet reference in tgw attachments

--- main.tf
+++ main.tf
@@ -80,20 +80,21 @@ resource "aws_ec2_transit_gateway" "this" {
 resource "aws_ec2_transit_gateway_vpc_attachment" "tgw-at-vpc-a" {
   transit_gateway_id = aws_ec2_transit_gateway.this.id
   vpc_id             = aws_vpc.vpc-a.id
-  subnet_ids         = [aws_subnet.subnet-1a]
+  subnet_ids         = [aws_subnet.subnet-1a.id]
   dns_support        = "enable"
 }
 
 resource "aws_ec2_transit_gateway_vpc_attachment" "tgw-at-vpc-b" {
   transit_gateway_id = aws_ec2_transit_gateway.this.id
   vpc_id             = aws_vpc.vpc-b.id
-  subnet_ids         = [aws_subnet.subnet-1b]
+  subnet_ids         = [aws_subnet.subnet-1b.id]
   dns_support        = "enable"
 }
 
 resource "aws_ec2_transit_gateway_vpc_attachment" "tgw-at-vpc-c" {
   transit_gateway_id = aws_ec2_transit_gateway.this.id
   vpc_id             = aws_vpc.vpc-b.id
-  subnet_ids         = [aws_subnet.subnet-1c]
+  subnet_ids         = [aws_subnet.subnet-1c.id]
   dns_support        = "enable"
 }
+
