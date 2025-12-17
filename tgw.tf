2844e798afe41c14ed140f05004d1e5070e2b991
Devron Tombacco <tombacco.devron@gmail.com>
2025-12-17 12:02:04 +0000

    refactor: move tgw resources to tgw.tf

---
+++ tgw.tf
@@ -0,0 +1,36 @@
+
+resource "aws_ec2_transit_gateway" "this" {
+  description     = "main-tgw"
+  amazon_side_asn = 64512
+
+  auto_accept_shared_attachments  = "disable"
+  default_route_table_association = "disable"
+  default_route_table_propagation = "disable"
+  dns_support                     = "enable"
+
+  tags = {
+    Name        = "main-tgw"
+    Environment = "prod"
+  }
+}
+
+resource "aws_ec2_transit_gateway_vpc_attachment" "tgw-at-vpc-a" {
+  transit_gateway_id = aws_ec2_transit_gateway.this.id
+  vpc_id             = aws_vpc.vpc-a.id
+  subnet_ids         = [aws_subnet.subnet-1a.id]
+  dns_support        = "enable"
+}
+
+resource "aws_ec2_transit_gateway_vpc_attachment" "tgw-at-vpc-b" {
+  transit_gateway_id = aws_ec2_transit_gateway.this.id
+  vpc_id             = aws_vpc.vpc-b.id
+  subnet_ids         = [aws_subnet.subnet-1b.id]
+  dns_support        = "enable"
+}
+
+resource "aws_ec2_transit_gateway_vpc_attachment" "tgw-at-vpc-c" {
+  transit_gateway_id = aws_ec2_transit_gateway.this.id
+  vpc_id             = aws_vpc.vpc-c.id
+  subnet_ids         = [aws_subnet.subnet-1c.id]
+  dns_support        = "enable"
+}
