da71d338fe108aab9cf29957a1230a97892f0f3f
Devron Tombacco <tombacco.devron@gmail.com>
2026-01-13 20:00:16 +0000

    feat: Add sg code for bastion host

---
+++ sg.tf
@@ -0,0 +1,23 @@
+
+resource "aws_security_group" "bastion_sg" {
+  name        = "bastion-sg"
+  description = "Allow inbound SSH from my IP + outbound all"
+  vpc_id      = aws_vpc.vpc_a.id
+
+  ingress {
+    description = "SSH from my IP"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    #cidr_blocks = ["${var.my_ip_address}/32"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = { Name = "bastion-sg" }
+}
