b57b045d4710d09c3e6b64e16d3416b47a193516
Devron Tombacco <tombacco.devron@gmail.com>
2026-01-14 19:24:27 +0000

    Feat: Add sg for ec2-c

--- sg.tf
+++ sg.tf
@@ -55,3 +55,38 @@ resource "aws_security_group" "private-ec2-b-sg" {
   }
 
 }
+
+resource "aws_security_group" "private-ec2-c-sg" {
+  name        = "private-ec2-c-sg"
+  description = "Allow inbound SSH and ICMP from Bastion"
+  vpc_id      = aws_vpc.vpc-c.id
+
+  ingress {
+    description = "Allow SSH from vpc-a"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "TCP"
+    cidr_blocks = ["10.0.0.0/16"]
+  }
+
+  ingress {
+    description = "Allow ICMP from all vpcs"
+    from_port   = -1
+    to_port     = -1
+    protocol    = "ICMP"
+    cidr_blocks = [
+      "10.0.0.0/16", # vpc-a
+      "10.1.0.0/16", # vpc-b
+      "10.2.0.0/16"  # vpc-c
+    ]
+  }
+
+  egress {
+    description = "allow all"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
