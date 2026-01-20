53803894c0228af036afc263da676a105036b180
Devron Tombacco <tombacco.devron@gmail.com>
2026-01-20 19:28:04 +0000

    Feat: add igw to vpc-on-prem

--- igw.tf
+++ igw.tf
@@ -6,3 +6,11 @@ resource "aws_internet_gateway" "igw" {
     Name = "igw"
   }
 }
+
+resource "aws_internet_gateway" "igw-on-prem" {
+  vpc_id = aws_vpc.vpc-on-prem.id
+
+  tags = {
+    Name = "igw-on-prem"
+  }
+}
